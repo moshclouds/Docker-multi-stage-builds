17469646cb04ef218a4b6cfdde885c5dcda39e33
Mohammad Shabeer <moshdev2213@gmail.com>
2025-07-28 12:21:32 +0530

    init

---
+++ src/main/java/com/moshclouds/DockerMultiStage/DockerMultiStageApplication.java
@@ -0,0 +1,13 @@
+package com.moshclouds.DockerMultiStage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMultiStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMultiStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moshclouds/DockerMultiStage/DockerMultiStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moshclouds.DockerMultiStage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerMultiStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
